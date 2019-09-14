b08e923ba9bbb36559b56c47ca5a906e30beabb2
Jason <zzc601596643@gmai.com>
2019-09-14 15:39:52 +0800

    @property

--- master_code.py
+++ master_code.py
@@ -1,21 +1,34 @@
+# -*- coding: utf-8 -*-
+class Screen(object):
+    
+    @property
+    def width(self):
+        return self._width
 
-class Student(object):
-    count = 0
+    @width.setter
+    def width(self,value):
+        self._width = value
 
-    def __init__(self, name):
-        self.name = name
-        Student.count += 1
+    @property
+    def height(self):
+        return self._height
+    
+    @height.setter
+    def height(self,value):
+        self._height = value
+
+    @property
+    def resolution(self):
+        self._resolution = self._height * self._width
+        return self._resolution
+
+
 # 测试:
-if Student.count != 0:
-    print('测试失败!')
+s = Screen()
+s.width = 1024
+s.height = 768
+print('resolution =', s.resolution)
+if s.resolution == 786432:
+    print('测试通过!')
 else:
-    bart = Student('Bart')
-    if Student.count != 1:
-        print('测试失败!')
-    else:
-        lisa = Student('Bart')
-        if Student.count != 2:
-            print('测试失败!')
-        else:
-            print('Students:', Student.count)
-            print('测试通过!')
+    print('测试失败!')
